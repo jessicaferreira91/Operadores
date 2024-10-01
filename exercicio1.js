bafce4220c86cd4ddc377c6f5f4c3866ec049cdd
Vinicius Leite <Vinicius010791@gmail.com>
2024-09-30 21:11:10 -0300

    exercicio de soma de variaveis

---
+++ exercicio1.js
@@ -0,0 +1,19 @@
+const primeirovalor=3
+const segundovalor=4
+const resultado=primeirovalor + segundovalor
+console.log ('primeiro calculo ', resultado)
+
+const valor1=3
+const valor2=5
+const valor3= valor1 * valor2
+const resultado2= valor3 / 2
+console.log ('calculo2 ', resultado2)
+
+let conta= (4-5) * -1
+console.log ('conta', conta)
+
+const valor11=234
+const valor22=5
+const reslta = valor11%valor22
+console.log ('reslta', reslta)
+
